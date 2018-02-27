04170dc58817f7cadcf2dcbe572d6e1e277e3c89
VincentWenForMe <1483319587@qq.com>
2018-02-27 23:12:51 +0800

    引入进度条

--- src/main.js
+++ src/main.js
@@ -7,9 +7,13 @@ import VueAwesomeSwiper from 'vue-awesome-swiper';
 import 'vue-awesome-swiper/node_modules/swiper/dist/css/swiper.css';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+// todo 全站进度条的引入以及部分数据的编写
+
 
 import axios from 'axios';
-
 Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false;
